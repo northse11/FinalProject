67923662a26a72b577f4ad61d1fc3c036f7698d6
Steven North <northse@mail.uc.edu>
2024-11-21 13:32:23 -0500

    Created test data inside of the context class

--- Data/FinalProjectContext.cs
+++ Data/FinalProjectContext.cs
@@ -14,6 +14,17 @@ namespace FinalProject.Data
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<TeamMember>().HasData(
+                new TeamMember { ID = 1, FirstName = "Steve", LastName = "North", Birthdate = new DateTime(2015, 12, 31), Program = "software", Year = "junior" });
+
+            modelBuilder.Entity<Hobby>().HasData(
+                new Hobby { HobbyID = 1, HobbyName = "Hobby", HobbyDescription = "Hobby....", HobbyAge = new DateTime(2015, 12, 31), HobbyActive = true, HobbyDifficulty = "Medium"}
+                );
+        }
+
 
         public DbSet<FinalProject.Models.TeamMember> TeamMember { get; set; } = default!;
         public DbSet<FinalProject.Models.Hobby> Hobby { get; set; } = default!;
---
+++ Migrations/20241121182734_MigrationWithTestData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinalProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigrationWithTestData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Hobby",
+                columns: new[] { "HobbyID", "HobbyActive", "HobbyAge", "HobbyDescription", "HobbyDifficulty", "HobbyName" },
+                values: new object[] { 1, true, new DateTime(2015, 12, 31, 0, 0, 0, 0, DateTimeKind.Unspecified), "Hobby....", "Medium", "Hobby" });
+
+            migrationBuilder.InsertData(
+                table: "TeamMember",
+                columns: new[] { "ID", "Birthdate", "FirstName", "LastName", "Program", "Year" },
+                values: new object[] { 1, new DateTime(2015, 12, 31, 0, 0, 0, 0, DateTimeKind.Unspecified), "Steve", "North", "software", "junior" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Hobby",
+                keyColumn: "HobbyID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "TeamMember",
+                keyColumn: "ID",
+                keyValue: 1);
+        }
+    }
+}
