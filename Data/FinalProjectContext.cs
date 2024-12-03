1a9bfc3e0888c7559a77024a1965364b1c9af519
Jackson Cooper <coope3jt@mail.uc.edu>
2024-12-03 12:50:55 -0500

    Hard coded context information

--- Data/FinalProjectContext.cs
+++ Data/FinalProjectContext.cs
@@ -19,7 +19,7 @@ namespace FinalProject.Data
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<TeamMember>().HasData(
                 new TeamMember { ID = 1, FirstName = "Steve", LastName = "North", Birthdate = new DateTime(2004, 08, 11), Program = "Software", Year = "junior" },
-                new TeamMember { ID = 2, FirstName = "Jack", LastName = "Cooper", Birthdate = new DateTime(2015, 12, 31), Program = "test", Year = "test" },
+                new TeamMember { ID = 2, FirstName = "Jack", LastName = "Cooper", Birthdate = new DateTime(2003, 06, 17), Program = "IT", Year = "test" },
                 new TeamMember { ID = 3, FirstName = "Isan", LastName = "Kalhan", Birthdate = new DateTime(2015, 12, 31), Program = "test", Year = "test" },
                 new TeamMember { ID = 4, FirstName = "Earl", LastName = "Schreck", Birthdate = new DateTime(2015, 12, 31), Program = "test", Year = "test" },
                 new TeamMember { ID = 5, FirstName = "Jon", LastName = "Soriano Sanjuan", Birthdate = new DateTime(2015, 12, 31), Program = "test", Year = "test" }
@@ -28,12 +28,18 @@ namespace FinalProject.Data
             modelBuilder.Entity<Hobby>().HasData(
                 new Hobby { HobbyID = 1, HobbyName = "Hobby", HobbyDescription = "Hobby....", HobbyAge = new DateTime(2015, 12, 31), HobbyActive = true, HobbyDifficulty = "Medium" }
                 );
+                new Hobby { HobbyID = 2, HobbyName = "Marching Band", HobbyDescription = "Playing the trumpet for the University of Cincinnati Marching Band.", HobbyAge = new DateTime(2022, 8, 01), HobbyActive = true, HobbyDifficulty = "Medium" }
+                );
 
             modelBuilder.Entity<FavoriteSong>().HasData(
                 new FavoriteSong { FavoriteSongID = 1, TeamMemberID = 1, SongName = "Bohemian Rhapsody", Artist = "Queen" }
                 );
+                new FavoriteSong { FavoriteSongID = 2, TeamMemberID = 2, SongName = "Invisible", Artist = "Duran Duran" }
+                );
             modelBuilder.Entity<FavoriteMovie>().HasData(
                 new FavoriteMovie { ID = 1, Title = "Inception", Director = "Christopher Nolan", ReleaseDate = new DateTime(2010, 7, 16), Genre = "Sci-Fi", Rating = 8.8 }
+                );
+                new FavoriteMovie { ID = 2, Title = "Star Wars: Episode V - The Empire Strikes Back", Director = "Irvin Kershner", ReleaseDate = new DateTime(1980, 5, 06), Genre = "Sci-Fi", Rating = 8.7 }
                 );
         }
 
